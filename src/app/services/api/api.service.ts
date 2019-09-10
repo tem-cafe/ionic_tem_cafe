57c7fe33b25ccd290b3d4fa08d40ce641d87ea77
LucasBrito <lucasbsilva29@gmail.com>
2019-09-09 23:10:33 -0300

    Criando api service e suas respectivas funções

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  public API_URL = 'https://api-tem-cafe.herokuapp.com/';
+  // private API_URL = 'http://localhost:3000';
+
+  constructor(
+    public httpClient: HttpClient,
+    public storage: Storage
+  ) { }
+
+  getHeader() {
+    const usuario = JSON.parse(localStorage.getItem('currentUser'));
+    return {
+      headers: {
+        Authorization: 'Bearer ' + (usuario.token || '')
+      }
+    };
+  }
+
+  postLogin(body) {
+    return this.httpClient.post(`${this.API_URL}/users/login`, body);
+  }
+
+}
