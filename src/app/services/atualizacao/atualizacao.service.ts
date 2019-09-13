0418c42dab3ea60861ef753ffee923aa80837395
LucasBrito <lucasbsilva29@gmail.com>
2019-09-13 16:23:45 -0300

    get para atualização

--- src/app/services/atualizacao/atualizacao.service.ts
+++ src/app/services/atualizacao/atualizacao.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AtualizacaoService {
 
-  constructor() { }
+  constructor(
+    private api: ApiService
+  ) { }
+
+  verificarAtualizacao() {
+    return this.api.get(`/verificarAtt/${this.api.VERSAO}`);
+  }
 }
